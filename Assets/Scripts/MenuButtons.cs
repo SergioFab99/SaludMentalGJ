e45055ef9d0ae935b7cbc96fb08a2d9f91be0150
Sergio Fabián <2021158559@talento.tls.edu.pe>
2025-05-31 01:11:45 -0500

    menu actualizado

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void EmpezarJuego()
+    {
+        SceneManager.LoadScene("Juego");
+    }
+
+    public void Opciones()
+    {
+        SceneManager.LoadScene("Opciones");
+    }
+
+    public void Salir()
+    {
+        // Esto funciona solo en compilaciones, no en editor
+        Application.Quit();
+        Debug.Log("Salir del juego");
+    }
+}
